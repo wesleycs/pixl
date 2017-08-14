ac7ac25c020c5bee951cd3c65be76a3dae9ee83b
Wesley Csendom <wesleycs@buffalo.edu>
2017-08-14 12:27:15 -0400

    adding content and styling css

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <html>
 <body>
-<h1> text </h1>
 <?php include 'users_online.php';?>
 <style>
 <?php include 'css/main.css'; ?>
@@ -16,6 +15,22 @@
     </ul>
 </div>
 
+<body>
+    <div class="container">
+        <div class="overlay">
+            <div class="info">
+                <h2 class="title">Hello World</h2>
+                <p class="artist">Designed by <a style="text-decoration: underline;" href="https://www.instagram.com/wcsendom/">Wesley Csendom<a></p>
+                <p class="description">A set of three 11"x17" one-color Risograph prints. Signed & numbered edition of 25.
+                $15.00 USD </p>
+                <input type="button" class="button" value="Buy">
+            </div>
+        </div>
+    </div>
+
+    <div class="counter">
+    <p class="count"><?php echo $count_user_online; ?> 👁️</p>
+    </div>
 <p>This is a test : <?php echo $count_user_online; ?> </p>
 </body>
 </html>
--- users_online.php
+++ users_online.php
@@ -5,11 +5,11 @@ $session    = session_id();
 $time       = time();
 $time_check = $time-300;     //We Have Set Time 5 Minutes
 
-$host     = "127.0.0.1";     // your Host name 
-$username = "root";          // your Mysql username 
-$password = "TrjR0330@";     // your Mysql password 
+$host     = "127.0.0.1";     // your Host name
+$username = "root";          // your Mysql username
+$password = "TrjR0330@";     // your Mysql password
 $db_name  = "test";          // your Database name
-$tbl_name = "online_users";  // Table name 
+$tbl_name = "online_users";  // Table name
 
 $con = mysqli_connect($host, $username, $password, $db_name);
 
@@ -23,7 +23,7 @@ $result = mysqli_query($con, $sql);
 $count = mysqli_num_rows($result);
 
 
-//if count is 0, then enter the values 
+//if count is 0, then enter the values
 
 if($count=="0") {
 $sql1 = "INSERT INTO $tbl_name(session, time) VALUES ('$session', '$time')";
@@ -39,13 +39,10 @@ $result2 = mysqli_query($con, $sql2);
 $sql3  = "SELECT * FROM $tbl_name";
 $result3 = mysqli_query($con, $sql3);
 $count_user_online = mysqli_num_rows($result3);
-echo "<b>Users Online : </b> $count_user_online ";
 
-//after 5 minutes, session will be deleted 
+//after 5 minutes, session will be deleted
 $sql4 = "DELETE FROM $tbl_name WHERE time <$time_check";
-$result4 = mysqli_query($sql4);
+$result4 = mysqli_query($con, $sql4);
 
 mysqli_close($con);
 ?>
-
-
