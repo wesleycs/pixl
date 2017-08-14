9232dc521332e1637bff09da92593e382bbfe4d0
Wesley Csendom <wesleycs@buffalo.edu>
2017-08-14 12:23:37 -0400

    css test

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <body>
 <h1> text </h1>
 <?php include 'users_online.php';?>
+<style>
+<?php include 'css/main.css'; ?>
+</style>
+
+<div>
+<ul>
+    <table>
+        <tr>
+            <th>PIXL</th>
+        </tr>
+        </table>
+    </ul>
+</div>
 
 <p>This is a test : <?php echo $count_user_online; ?> </p>
 </body>
