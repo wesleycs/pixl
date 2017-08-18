5585bcfff0155c47c18cd153eb2358f52ed535a2
Wesley Csendom <wesleycs@buffalo.edu>
2017-08-18 11:32:06 -0400

    Updating submit page

---
+++ qq8c/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Pixl-Gallery </title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include '../css/qq8c.css'; ?>
+<style>
+</style>
+</head>
+
+<body>
+
+
+<div>
+
+<ul>
+    <table>
+        <tr>
+            <th><span><a href="../">PIXL</a></span></th>
+        </tr>
+        </table>
+    </ul>
+</div>
+
+<body>
+
+<div class="submit">
+<p>Pixl is a curated art gallery and retail space.<br><br>
+   Pieces are displayed in the order that they are received.<br><br>
+   When the current work is
+   sold a new piece will take its place.<br><br>
+   If you would like to feature your work on Pixl please click <a href="../contact">here</a><br><br>
+   Check back often to discover new artists and enjoy new works.
+   </p>
+</div>
+
+
+</body>
+
+
+</html>
